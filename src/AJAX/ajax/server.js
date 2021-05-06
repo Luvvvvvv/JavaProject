8d7dc547931e8a8b5bab643ddf4f0d0899769aaa
Luv <651828308@qq.com>
2021-05-06 16:30:36 +0800

    ajax

---
+++ src/AJAX/ajax/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.get('/server', (request, response) => {
+  //设置响应头，允许跨域
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.send('hello ajax i');
+});
+
+
+app.all('/json-server', (request, response) => {
+  //设置响应头，允许跨域
+  response.setHeader('Access-Control-Allow-Origin', '*')
+
+  response.setHeader('Access-Control-Allow-Header', '*')
+
+  const data = {
+    name: 'luvvhonvv'
+  };
+  let str=JSON.stringify(data);
+
+  response.send(str);
+});
+
+
+app.listen(8000, () => {
+  console.log('服务已启动');
+});
